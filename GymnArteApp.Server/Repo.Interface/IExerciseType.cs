9ba9076fda7071d59b14d4174b0a754228c7fe41
joaopereirarosa <joao.pereira.rosa@prologica.pt>
2026-03-17 22:27:18 +0000

    API - Repo Interfaces

--- GymnArteApp.Server/Repo.Interface/IExerciseType.cs
+++ GymnArteApp.Server/Repo.Interface/IExerciseType.cs
@@ -2,5 +2,10 @@
 {
     public interface IExerciseType
     {
+        Task<IEnumerable<Models.ExerciseType>> GetAllExerciseTypesAsync();
+        Task<Models.ExerciseType> GetExerciseTypeByIdAsync(int id);
+        Task<Models.ExerciseType> CreateExerciseTypeAsync(Models.ExerciseType exerc);
+        Task<Models.ExerciseType> UpdateExerciseTypeAsync(int id, Models.ExerciseType exerc, string token);
+        Task<bool> DisableExerciseTypeAsync(int id, string token);
     }
 }
--- GymnArteApp.Server/Repo.Interface/INotifications.cs
+++ GymnArteApp.Server/Repo.Interface/INotifications.cs
@@ -2,5 +2,9 @@
 {
     public interface INotifications
     {
+        Task<Models.Notifications> GetNotificationByIdAsync(int id);
+        Task<Models.Notifications> GetNotificationByUserIdAsync(int id);
+        Task<Models.Notifications> CreateNotificationAsync(Models.Notifications notif, string token);
+        Task<bool> DeleteNotificationsAsync(int id, string token);
     }
 }
