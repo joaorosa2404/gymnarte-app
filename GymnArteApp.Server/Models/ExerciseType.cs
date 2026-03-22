3cd8808846718db21467981f4f2dfe21156143fb
João Rosa <joao.amador.rosa@gmail.com>
2026-03-22 19:10:01 +0000

    fix model logic for scaffold

--- GymnArteApp.Server/Models/ExerciseType.cs
+++ GymnArteApp.Server/Models/ExerciseType.cs
@@ -2,12 +2,18 @@
 {
     public class ExerciseType
     {
-        //id; nome; descricao; dtcriacao;
         public int ExerciseTypeId { get; set; }
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
+
+        // Auditoria
+        public int? UpdatedUserId { get; set; }
         public User? UpdatedUser { get; set; }
+
+        // Navegações inversas
+        public ICollection<Exercise> Exercises { get; set; } = [];
+        public ICollection<TrainingPlan> TrainingPlans { get; set; } = [];
     }
 }
--- GymnArteApp.Server/obj/Debug/net8.0/GymnArteApp.Server.AssemblyInfo.cs
+++ GymnArteApp.Server/obj/Debug/net8.0/GymnArteApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymnArteApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aaf1321ef6e4d5be56ad2116e848af3acb04f2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69df8ee8511445fc14d4898508e1deed40cf5816")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymnArteApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymnArteApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
